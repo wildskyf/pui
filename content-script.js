add1abe3d5d844ccda357bc14765da1a0f3ade8b
wildskyf <poppin.wildsky+gh@gmail.com>
2018-04-10 23:57:00 +0800

    fix duplicate bug when clicking command + f lots of times

--- content-script.js
+++ content-script.js
@@ -1,5 +1,4 @@
 var foxmosa_js = {
-  $foxmosa_run: null,
   fox: {
     run: "foxmosa-iwy-run",
     find: "foxmosa-iwy-find"
@@ -7,16 +6,16 @@ var foxmosa_js = {
 
   init: () => {
     var me = foxmosa_js;
-    if (document.querySelector('#' + me.fox.run)) return;
-
     me.createRunningFox();
-    me.run();
     me.appearWhenFind();
   },
 
   appear: id => {
     var me = foxmosa_js;
-    var img = me.$foxmosa_run = document.createElement("IMG");
+
+    if (document.querySelector('#' + me.fox[id])) return;
+
+    var img = document.createElement("IMG");
     img.classList.add('foxmosa-iwy');
 
     switch(id) {
@@ -34,12 +33,10 @@ var foxmosa_js = {
 
   createRunningFox: () => {
     var me = foxmosa_js;
-    document.body.append(me.appear('run'));
-  },
+    var $fox_run = me.appear('run');
 
-  run: () => {
-    var me = foxmosa_js;
-    var { $foxmosa_run } = me;
+    if (!$fox_run) return;
+    document.body.append($fox_run);
 
     setTimeout( () => {
       $foxmosa_run.style.right = '-192px';
@@ -50,9 +47,11 @@ var foxmosa_js = {
     var me = foxmosa_js;
     document.addEventListener('keydown', e => {
       // ctrl/command + c
-      if (e.keyCode == 70 && (e.ctrlKey || e.metaKey)){
+      if (e.keyCode == 70 && (e.ctrlKey || e.metaKey)) {
 
         var $fox_find = me.appear('find');
+
+        if (!$fox_find) return;
         document.body.append($fox_find);
 
         $fox_find.addEventListener('click', () => {
